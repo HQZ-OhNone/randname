a412d262a915f498057619e33ba0fcc5c5a1894e
HQZ-OhNone <ohnone_hqz@outlook.com>
2026-05-03 00:26:04 +0800

    3.0.2_import.complete_licence

---
+++ code/import-names.py
@@ -0,0 +1,48 @@
+"""
+Copyright (C) 2026 HQZ-OhNone <ohnone_hqz@outlook.com>
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+"""
+读取:
+    randname/config/
+    randname/config/names.json
+执行:
+    将 names.json 读取为字典
+    打印测试
+"""
+from pathlib import Path
+import json
+
+# 设定文件路径
+path_code = Path(__file__).parent
+path_root = path_code.parent
+path_config = path_root / "config"
+path_names = path_config / "names.json"
+
+# 判断所需文件是否齐全
+passon = True
+if path_config.exists():
+    print("找到: config")
+elif not path_names.exists():
+    print("names.json 不存在！")
+    print("退出")
+    passon = False
+else:
+    print("config 文件夹不存在！")
+    print("退出")
+    passon = False
+print("找到: names.json")
+
+# 如果文件齐全，则执行：
+if passon:
+    # 读取JSON
+    names = json.loads(path_names.read_text(encoding="utf-8"))
+    print("已导入: names.json")
+    print(names)
+
